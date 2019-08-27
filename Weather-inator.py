c12933269667fce8716d996b2d9d689894196658
David Whipple <cubbieco@gmail.com>
2019-08-26 20:03:47 -0600

    Working on the 5 day forecast

--- Weather-inator.py
+++ Weather-inator.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-import requests, json
-from guizero import App,Box,Picture,Text
+from datetime import date
+from datetime import datetime
+from datetime import time
+from datetime import timedelta
+from guizero import App
+from guizero import Box
+from guizero import Picture
+from guizero import Text
+import json
 from pathlib import Path
 from pprint import pprint
-from datetime import datetime, date, time, timedelta
+import requests
 
 home = str(Path.home())
 
 # don't exactly want to publish my apiKey on github
-f = open(home+"/apiKey.txt","r")
+f = open(home + "/apiKey.txt", "r")
 if f.mode == "r":  # file opened
-	apiKey = f.read()
+    apiKey = f.read()
 else:
-	apiKey = ""
+    apiKey = ""
 	
 
 apiKey = apiKey.strip()
@@ -23,91 +30,128 @@ forecastUrl = baseUrl + "forecast?appid=" + apiKey + "&q=" + cityName
 
 preferred = "Fahrenheit"
 
+#Need some way to pic the most serious picture for a day
+weatherSeriousness = ["clear sky","few clouds","scattered clouds","broken clouds",
+    "show rain","rain","thunderstorm","snow","mist"]
 
+def getWeatherSeriousness(seriousness):
+    switcher = {
+        "clear sky":0,
+        "few clouds":1,
+        "scattered clouds":2,
+        "broken clouds":3,
+        "shower rain":4,
+        "rain":5,
+        "thunderstorm":6,
+        "snow":7,
+        "mist":8
+    }
+    return switcher.get(seriousness, 3)
+    
 
 def descToFilename(desc):
-	switcher = {
-		"clear sky":"day_clear.png",
-		"few clouds":"day_partial_cloud.png",
-		"scattered clouds":"cloudy",
-		"broken clouds":"angry_clouds.png",
-		"shower rain":"day_rain.png",
-		"rain":"rain.png",
-		"thunderstorm":"thunder.png",
-		"snow":"snow.png",
-		"mist":"mist.png"
-	}
-	return switcher.get(desc,"tornado.png")
-	
+    switcher = {
+        "clear sky":"day_clear.png",
+        "few clouds":"day_partial_cloud.png",
+        "scattered clouds":"cloudy",
+        "broken clouds":"angry_clouds.png",
+        "shower rain":"day_rain.png",
+        "rain":"rain.png",
+        "thunderstorm":"thunder.png",
+        "snow":"snow.png",
+        "mist":"mist.png"
+    }
+    return switcher.get(desc, "tornado.png")
 
 
 def getForecast(Url):
-	response = requests.get(Url)
-	forecast = response.json()
-	return forecast
+    response = requests.get(Url)
+    forecast = response.json()
+    return forecast
 
 # openweather uses kelvin	
 def kelvinToFahrenheit(temperature):
-	fahrenheit = (temperature - 273.15) * 9 / 5 + 32
-	return fahrenheit
+    fahrenheit = (temperature - 273.15) * 9 / 5 + 32
+    return fahrenheit
 	
 # openweather uses kelvin
 def kelvinToCelcius(temperature):
-	celcius = temperature - 273.15
-	return celcius
+    celcius = temperature - 273.15
+    return celcius
 	
 def getCurrentWeather():
     global weatherUrl
     forecast = getForecast(weatherUrl)
     #make sure we got some data
     if forecast["cod"] == 200:
-            temperature = forecast["main"]["temp"]
-            humidity = forecast["main"]["humidity"]
-            desc = forecast["weather"][0]["description"]
+        temperature = forecast["main"]["temp"]
+        humidity = forecast["main"]["humidity"]
+        desc = forecast["weather"][0]["description"]
     else:
-            temperature = 0
-            humidity = 0
-            desc = "None"
-    print("Temperature:",temperature,"Hum:",humidity,"Desc:"+desc)
-    return [temperature,humidity,desc]
+        temperature = 0
+        humidity = 0
+        desc = "None"
+    print("Temperature:", temperature, "Hum:", humidity, "Desc:" + desc)
+    return [temperature, humidity, desc]
 
 def getCurrentForecast():
     global forecastUrl
     forecast = getForecast(forecastUrl)
     #make sure we got some data
-    if forecast["cod"] == "200":
-        print("hi")
-        #pprint.pprint(forecast)
-    else:
+    if forecast["cod"] != "200":
         return false
     timezone = forecast["city"]["timezone"]
-    print(forecast['list'][0])
-    forecastList = []
+
+    midnightTonight = datetime.combine(datetime.today(), time.min) + timedelta(days=1)
+    # set up some variables to figure out our forecast
+    midnights = [midnightTonight, midnightTonight + timedelta(days=1), midnightTonight + timedelta(days=2),
+        midnightTonight + timedelta(days=3), midnightTonight + timedelta(days=4), midnightTonight + timedelta(days=5)]
+    lowTemps = [400, 400, 400, 400, 400, 400, 400]
+    highTemps = [0, 0, 0, 0, 0, 0, 0]
+    humidities = [0, 0, 0, 0, 0, 0, 0]
+    seriousnesses = [0, 0, 0, 0, 0, 0, 0]
+    weatherNow = getCurrentWeather()
+    lowTemps[0] = highTemps[0] = weatherNow[0]
+    humidities[0] = weatherNow[1]
+    seriousnesses[0] =getWeatherSeriousness(weatherNow[2])
+    
+    thisDay = 0
+    
     #need to iterate through this and figure out the min / max temperature per day
     for f in forecast['list']:
         thisDt = datetime.fromtimestamp(f['dt'])
         thisTemp = f['main']['temp']
         thisHum = f['main']['humidity']
         thisFor = (f['weather'][0]['description'])
-        forecastList.append([thisDt,thisTemp,thisHum,thisFor])
+        thisSeriousness = getWeatherSeriousness(thisFor)
         
-    pprint(forecastList)
-    midnight = datetime.combine(datetime.today(), time.min) + timedelta(days=1)
-    print(midnight)
+        if thisDt > midnights[thisDay]:
+            thisDay += 1
+
+        lowTemps[thisDay] = min(thisTemp, lowTemps[thisDay])
+        highTemps[thisDay] = max(thisTemp, highTemps[thisDay])
+        humidities[thisDay] = max(thisHum, humidities[thisDay])
+        seriousnesses[thisDay] = max(thisSeriousness, seriousnesses[thisDay])
         
+        
+    pprint(highTemps)
+    pprint(lowTemps)
+    print(humidities)
+    pprint(seriousnesses)
+    
     
     
     
 
-app = App(title="Weather-inator", layout="grid" )
+app = App(title="Weather-inator", layout="grid")
 
 #boxes
-tempBox = Box(app, grid=[0,0,5,1], border=True, width=500, height=250)
-day1Box = Box(app, grid=[0,1], border=True, width=100, height=250)
-day2Box = Box(app, grid=[1,1], border=True, width=100, height=250)
-day3Box = Box(app, grid=[2,1], border=True, width=100, height=250)
-day4Box = Box(app, grid=[3,1], border=True, width=100, height=250)
-day5Box = Box(app, grid=[4,1], border=True, width=100, height=250)
+tempBox = Box(app, grid=[0, 0, 5, 1], border=True, width=500, height=350)
+day1Box = Box(app, grid=[0, 1], border=True, width=100, height=150)
+day2Box = Box(app, grid=[1, 1], border=True, width=100, height=150)
+day3Box = Box(app, grid=[2, 1], border=True, width=100, height=150)
+day4Box = Box(app, grid=[3, 1], border=True, width=100, height=150)
+day5Box = Box(app, grid=[4, 1], border=True, width=100, height=150)
 
 
 
@@ -132,15 +176,15 @@ def updateWeather():
     weather = getCurrentWeather()
 
     if preferred == "Fahrenheit":
-            ourTemp = kelvinToFahrenheit(weather[0])
-            ourTemp = str(round(ourTemp,2)) +u"\u00b0F"
+        ourTemp = kelvinToFahrenheit(weather[0])
+        ourTemp = str(round(ourTemp, 2)) + u"\u00b0F"
     else:
-            ourTemp = kelvinToCelcius(weather[0])
-            ourTemp = str(round(ourTemp,2)) +u"\u00b0C"
+        ourTemp = kelvinToCelcius(weather[0])
+        ourTemp = str(round(ourTemp, 2)) + u"\u00b0C"
 
-    tempText.value = "Temperature: " +ourTemp
-    humidBox.value = "Humidity: " + str(round(weather[1],2)) + "%"
-    picBox.image="pics/"+descToFilename(weather[2])
+    tempText.value = "Temperature: " + ourTemp
+    humidBox.value = "Humidity: " + str(round(weather[1], 2)) + "%"
+    picBox.image = "pics/" + descToFilename(weather[2])
 
 def updateForecast():
     global preferred
@@ -150,7 +194,7 @@ updateWeather()
 updateForecast()
 
 
-app.repeat((10*60*1000),updateWeather) #update every 10 minutes
+app.repeat((10 * 60 * 1000),updateWeather) #update every 10 minutes
 app.repeat((60*60*1000),updateForecast) #update every hour
 
 
